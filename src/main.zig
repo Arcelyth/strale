e6505b53ec45665a63b82dbca66ed47d23a11340
Arcelyth <arcelyth@tutamail.com>
2026-07-04 14:10:55 +0800

    flat export symbols

--- src/main.zig
+++ src/main.zig
@@ -1,4 +1,11 @@
-pub const strale = @import("strale.zig");
+const strale = @import("strale.zig");
+pub const Strale = strale.Strale;
+pub const StraleBytes = strale.StraleBytes;
+pub const StraleUtf8 = strale.StraleUtf8;
+pub const StraleAtomic = strale.StraleAtomic;
+pub const StraleUtf8Atomic = strale.StraleUtf8Atomic;
+pub const Format = strale.Format;
+pub const Atomicity = strale.Atomicity;
 
 const bd = @import("buffer_deque.zig");
 pub const BufferDeque = bd.BufferDeque; 
